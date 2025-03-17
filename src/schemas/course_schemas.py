5da056267e2e8273d440acbbf347b6b6157fdf3b
tinchomorilla <mmorilla@fi.uba.ar>
2025-03-17 18:23:22 -0300

    Add Pydantic schemas for Course model

---
+++ src/schemas/course_schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class CourseBase(BaseModel):
+    name: str
+    description: str
+
+# Pydantic model for returning the course data with ID
+class Course(CourseBase):
+    id: int
+
+    class Config:
+        from_attributes = True  # Tells Pydantic to treat the SQLAlchemy model as a dictionary
