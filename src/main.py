e66106a7ab913ad371b0ee6865ecbbd2ff09a01b
tinchomorilla <mmorilla@fi.uba.ar>
2025-03-20 18:30:15 -0300

    Include Health check endpoint

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from src.controllers.course_controller import router as courses_router
 from src.database.db_init import initialize_database
 from src.errors.exception_handler import configure_exception_handlers
+from src.controllers.health import router as health_router
 
 
 app = FastAPI()
@@ -11,6 +12,10 @@ app = FastAPI()
 # Initialize the database (create tables if they don't exist)
 initialize_database()
 
+# Include Health check endpoint
+app.include_router(health_router, tags=["health"])
+
+# Include Courses router
 app.include_router(courses_router, prefix="/courses", tags=["courses"])
 
 # Register custom exception handlers
