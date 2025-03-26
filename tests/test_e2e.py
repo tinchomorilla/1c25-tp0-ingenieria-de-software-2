06ed3e505307b008f74235918d2b8168b9599f54
tinchomorilla <mmorilla@fi.uba.ar>
2025-03-26 17:35:59 -0300

    Update tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -11,52 +11,71 @@ def test_create_course():
     response = client.post(
         "/courses/",
         json={
-            "name": "viva",
+            "title": "viva",
             "description": "scaloni",
         },
     )
     assert response.status_code == 201
-    course_id = response.json()["id"]
-    assert uuid.UUID(course_id)
-    assert response.json() == {"id": course_id, "name": "viva", "description": "scaloni"}
+    course_data = response.json()["data"]  # Access the "data" object in the POST response
+    course_id = course_data["id"]   # Extract the "id" from the "data" object
+
+    assert uuid.UUID(course_id)  
+    assert course_data == {"id": course_id, "title": "viva", "description": "scaloni"}
 
 def test_get_courses():
     response = client.post(
         "/courses/",
         json={
-            "name": "NeymarJr",
+            "title": "NeymarJr",
             "description": "es GOD",
         },
     )
-    course_id = response.json()["id"]
+    assert response.status_code == 201
+    course_data = response.json()["data"]  
+    course_id = course_data["id"]  
     response = client.get("/courses/")
     assert response.status_code == 200
-    courses = response.json()
-    assert any(course["id"] == course_id and course["name"] == "NeymarJr" and course["description"] == "es GOD" for course in courses)
+    courses = response.json()["data"]  
+
+    # Check if the created course exists in the list of courses
+    assert any(
+        course["id"] == course_id and course["title"] == "NeymarJr" and course["description"] == "es GOD"
+        for course in courses
+    )
 
 def test_get_course_by_id():
     response = client.post(
         "/courses/",
         json={
-            "name": "AM2",
+            "title": "AM2",
             "description": "ACERO",
         },
     )
-    course_id = response.json()["id"]
+    assert response.status_code == 201
+    course_data = response.json()["data"]  
+    course_id = course_data["id"]  
+
     response = client.get(f"/courses/{course_id}")
     assert response.status_code == 200
-    assert response.json() == {"id": course_id, "name": "AM2", "description": "ACERO"}
+    course_data = response.json()["data"]  
+
+    # Assert the course data matches the expected values
+    assert course_data == {"id": course_id, "title": "AM2", "description": "ACERO"}
 
 def test_delete_course_by_id():
     response = client.post(
         "/courses/",
         json={
-            "name": "AM2",
+            "title": "AM2",
             "description": "maulhardtEsGod",
         },
     )
-    course_id = response.json()["id"]
+    assert response.status_code == 201
+    course_data = response.json()["data"]  
+    course_id = course_data["id"]  
+
     response = client.delete(f"/courses/{course_id}")
     assert response.status_code == 204
+
     response = client.get(f"/courses/{course_id}")
     assert response.status_code == 404
